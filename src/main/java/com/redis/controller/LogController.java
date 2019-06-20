20fbc5f88ba9831dd7e07168947f9096aae0dc10
wangkun <wangkun2@tuniu.com>
2019-06-20 19:07:49 +0800

    update

--- src/main/java/com/redis/controller/LogController.java
+++ src/main/java/com/redis/controller/LogController.java
@@ -6,10 +6,11 @@ import com.redis.mq.RedisMessageListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.io.ByteArrayInputStream;
@@ -46,8 +47,11 @@ public class LogController {
 
 
     @RequestMapping(value = "/getstudent", method = RequestMethod.GET)
-    public void getStudent(@RequestParam("id") String id) {
-        logger.info("id = " + id);
+    public void getStudent() {
+        logger.debug("debug level");
+        logger.info("info level");
+        logger.warn("warn level");
+        logger.error("error level");
     }
 
     @RequestMapping(value = "updatelevel", method = RequestMethod.GET)
@@ -62,5 +66,28 @@ public class LogController {
         InputStream is = new ByteArrayInputStream(logback.getBytes("utf-8"));
         configurator.doConfigure(is);
         is.close();
+    }
+
+    /**
+     * 动态更新log4j日志级别
+     * @param xml logback.xml文件内容
+     * @return
+     */
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String updateLogLevel(@RequestBody String xml) {
+        logger.info("param = {}", xml);
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        context.getStatusManager().clear();
+        context.reset();
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(context);
+        try {
+            InputStream is = new ByteArrayInputStream(xml.getBytes("utf-8"));
+            configurator.doConfigure(is);
+            is.close();
+        } catch (Exception e) {
+            logger.error("update log level error", e);
+        }
+        return "{\"success\":\"true\"}";
     }
 }
